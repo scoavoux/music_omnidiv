10ff65210a189b08625aa9a144399e2aa0aee210
Samuel Coavoux <samuel.coavoux@gmail.com>
2017-08-11 12:09:56 +0200

    ajouté variable aire urbaine

--- scripts/localisation.R
+++ scripts/localisation.R
@@ -14,6 +14,9 @@ load("data/cities_coordinates.RData")
 ### http://www.insee.fr/fr/statistiques/2388413
 ### Téléchargé dans data/INSEE_revenu_communes
 
+## Jeu de données Aires urbaines/taille ville
+### https://www.insee.fr/fr/information/2115011
+
 ###### Prepare INSEE data ######
 ## Récupérer les codes des variables
 dico <- lapply(list.files("data/INSEE_revenu_communes", full.names = TRUE)[1:2],
@@ -32,9 +35,24 @@ r <- lapply(list.files("data/INSEE_revenu_communes", full.names = TRUE)[1:2],
             })
 
 r <- full_join(r[[1]], r[[2]], by = c("CODGEO", "LIBGEO"))
+r <- select(r, CODGEO, revenu_median = Q213)
 
-cities <- left_join(cities, r, by = c("codeInsee" = "CODGEO")) %>% 
-  rename(revenu_median = Q213)
+## Importer les données AUR
+s <- read_xls("data/INSEE_AUR/AU2010 au 01-01-2017.xls", sheet = 2, skip = 5)
+s_dic <- c(`111` = "Commune appartenant à un grand pôle (10 000 emplois ou plus)",
+           `112` = "Commune appartenant à la couronne d'un grand pôle",
+           `120` = "Commune multipolarisée des grandes aires urbaines",
+           `211` = "Commune appartenant à un moyen pôle (5 000 à moins de 10 000 emplois)",
+           `212` = "Commune appartenant à la couronne d'un moyen pôle",
+           `221` = "Commune appartenant à un petit pôle (de 1 500 à moins de 5 000 emplois)",
+           `222` = "Commune appartenant à la couronne d'un petit pôle",
+           `300` = "Autre commune multipolarisée",
+           `400` = "Commune isolée hors influence des pôles")
+s <- select(s, CODGEO, AUR = CATAEU2010) %>% 
+  mutate(AUR = s_dic[AUR] %>% factor(levels = s_dic))
+
+cities <- left_join(cities, r, by = c("codeInsee" = "CODGEO")) %>%
+  left_join(s, by = c("codeInsee" = "CODGEO"))
 
 save(cities, file = "data/cities.RData")
-rm(dico, r)
+rm(dico, r, s)
